655f762d7070c52f8d456b9ab068826f16032ed4
HernandezEduin <eduin.ee08@nycu.edu.tw>
2025-08-12 17:35:08 +0800

    Feed NLQ Data
    Added a new class that provide the QA dataset in batch form. This will
    serve as a replacement to RelationEntityBatcher which works with
    triplets. QuestionBatcher on the other hand works with Natural Language
    Questions. Transformer.py now serves as an example file on how to work
    with this batcher.
    
    Update

---
+++ code/data/feed_nlq_data.py
@@ -0,0 +1,142 @@
+import os
+
+import numpy as np
+import tensorflow as tf
+from transformers import TFAutoModel, AutoTokenizer
+
+from code.data.utils import load_dictionary, load_qa_data, ids_to_embeddings_tf
+
+from typing import Generator, Dict, Any, Tuple
+
+class QuestionBatcher():
+    """
+    Batches Natural Language Questions (NLQ) for training and evaluation.
+    Includes tokenization and embedding of questions, source entity (starting position),
+    and answer entity.
+    """
+    def __init__(
+            self, 
+            input_dir: str,
+            batch_size: int, 
+            question_tokenizer_name: str,
+            cached_QAMetaData_path: str,
+            raw_QAData_path: str,
+            mode: str = "train",
+            force_data_prepro: bool = False,
+        ) -> None:
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+        self.batch_size = batch_size
+
+        ent2id, rel2id, id2ent, id2rel = load_dictionary(input_dir)
+        self.entity_vocab = ent2id
+        self.relation_vocab = rel2id
+        self.id2entity = id2ent
+        self.id2relation = id2rel
+
+        self.train_df, self.dev_df, self.test_df, self.train_metadata = load_qa_data(
+            cached_metadata_path=cached_QAMetaData_path,
+            raw_QAData_path=raw_QAData_path,
+            question_tokenizer_name=question_tokenizer_name,
+            answer_tokenizer_name=question_tokenizer_name, 
+            entity2id=ent2id,
+            relation2id=rel2id,
+            logger=None,
+            force_recompute=force_data_prepro,
+        )
+        self.set_mode(mode)
+
+        # TODO: See if we can reduce this to 1 model
+        self.question_embedder = TFAutoModel.from_pretrained(question_tokenizer_name, from_pt=False) # Text to Embeddings
+        self.question_tokenizer = AutoTokenizer.from_pretrained(question_tokenizer_name) # Text to Tokens and vice versa
+
+    def set_mode(self, mode: str) -> None:
+        """
+        Change the mode of the batcher and assigns the respective evaluation dataframe.
+        """
+        assert mode in ['train', 'dev', 'test'], "Mode must be one of ['train', 'dev', 'test']"
+        self.mode = mode
+        if mode == 'train':
+            self.eval_df = self.train_df
+        elif mode == 'dev':
+            self.eval_df = self.dev_df
+        else:
+            self.eval_df = self.test_df
+
+    def get_mode(self) -> str:
+        """
+        Get the current mode of the batcher.
+        """
+        return self.mode
+
+    def yield_next_batch_train(self) -> Generator[Tuple[list, tf.Tensor, np.ndarray, np.ndarray], None, None]:
+        assert self.mode == 'train', "Batcher is not in training mode"
+        while True:
+            batch_idx = np.random.randint(0, len(self.eval_df), size=self.batch_size)  # randomly samples batch indices
+            batch = self.eval_df.iloc[batch_idx]                                       # extract dataframes with the sampled indices
+            questions = batch['Question'].tolist()                                      # extract the list of questions
+            source_ent = batch["Query-Entity"].to_numpy(dtype=int)                      # extract the starting node, already in id format
+            answers = batch['Answer-Entity'].to_numpy(dtype=int)                        # already in id format
+
+            # NOTE: Addiontal keys include {"Hops", "Paths"}
+
+            # convert question tokens into embeddings of sizes [batch, hidden]
+            question_embeddings = ids_to_embeddings_tf(
+                token_id_batches=questions,
+                model=self.question_embedder,
+                pad_id=0,                  # adjust if your PAD differs
+                add_special_tokens=True,   # set False if your IDs already include [CLS]/[SEP]
+                cls_id=101,
+                sep_id=102,
+                max_length=128,
+            )
+
+            yield questions, question_embeddings, source_ent, answers
+
+    def yield_next_batch_test(self) -> Generator[Tuple[list, tf.Tensor, np.ndarray, np.ndarray], None, None]:
+        remaining_questions = len(self.eval_df)                                         # remaining number of questions for evaluation (starting value)
+        current_idx = 0
+        while True:
+            if remaining_questions - self.batch_size > 0:                                 # so long as the number of remaining questions is bigger than the batch size
+                batch_idx = np.arange(current_idx, current_idx+self.batch_size)         # extract the batch indices
+                current_idx += self.batch_size                                          # increment the current index counter
+                remaining_questions -= self.batch_size                                    # reduce the number of remaining questions
+            else:
+                batch_idx = np.arange(current_idx, len(self.eval_df))                 # extract the batch indices from current position to maximum
+                remaining_questions = 0                                                   # set remaining question value to 0
+
+            batch = self.eval_df.iloc[batch_idx]                                       # extract dataframes with the sampled indices
+            questions = batch['Question'].tolist()                                      # extract the list of questions
+            source_ent = batch["Query-Entity"].to_numpy(dtype=int)                      # extract the starting node, already in id format
+            answers = batch['Answer-Entity'].to_numpy(dtype=int)                        # already in id format
+
+            # convert question tokens into embeddings of sizes [batch, hidden]
+            question_embeddings = ids_to_embeddings_tf(
+                token_id_batches=questions,
+                model=self.question_embedder,
+                pad_id=0,                  # adjust if your PAD differs
+                add_special_tokens=True,   # set False if your IDs already include [CLS]/[SEP]
+                cls_id=101,
+                sep_id=102,
+                max_length=128,
+            )
+
+            yield questions, question_embeddings, source_ent, answers
+
+    def translate_entities(self, entity_ids: np.ndarray) -> list:
+        """
+        Translate entity IDs into their corresponding entity names.
+        """
+        return [self.id2entity.get(eid, "Unknown") for eid in entity_ids]
+
+    def translate_relations(self, relation_ids: np.ndarray) -> list:
+        """
+        Translate relation IDs into their corresponding relation names.
+        """
+        return [self.id2relation.get(rid, "Unknown") for rid in relation_ids]
+
+    def translate_questions(self, questions: list) -> list:
+        """
+        Translate question IDs into their corresponding question texts.
+        """
+        return [self.question_tokenizer.decode(question) for question in questions]
--- code/model/transformer.py
+++ code/model/transformer.py
@@ -1,15 +1,14 @@
-import os
 import argparse
 
-from tqdm import tqdm
-
-from transformers import AutoTokenizer, TFAutoModel
-import code.data.utils as data_utils
+from code.data.feed_nlq_data import QuestionBatcher
 
 def get_args() -> argparse.Namespace:
     ap = argparse.ArgumentParser()
 
     'Datasets & File Paths'
+    ap.add_argument('--batch_size', type=int, default=32, help='Batch size for training and evaluation (default: 32)')
+    ap.add_argument('--mode', type=str, default='train', choices=['train', 'dev', 'test'], help='Mode for the batcher (default: train)')
+
     # KG Dataset
     ap.add_argument('--data_dir', type=str, default="./datasets/data_preprocessed/kinshiphinton", help='Root directory for KG triples and metadata (default: ./data/FB15k)')
 
@@ -28,54 +27,22 @@ if __name__ == "__main__":
 
     args = get_args()
 
-    # Load the KGE Dictionaries
-    ent2id, rel2id, id2ent, id2rel = data_utils.load_dictionary(args.data_dir)
-
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-    # question_tokenizer = AutoTokenizer.from_pretrained(args.question_tokenizer_name)
-    question_encoder = TFAutoModel.from_pretrained(args.question_tokenizer_name, from_pt=False)
-
-    # TODO: Check if it is necessary to convert the entity/relation to ids in the MINERVA algorithm
-    train_df, dev_df, test_df, train_metadata = data_utils.load_qa_data(
-        cached_metadata_path=args.cached_QAMetaData_path,
-        raw_QAData_path=args.raw_QAData_path,
-        question_tokenizer_name=args.question_tokenizer_name,
-        answer_tokenizer_name=args.answer_tokenizer_name, 
-        entity2id=ent2id,
-        relation2id=rel2id,
-        logger=None,
-        force_recompute=args.force_data_prepro,
+    batcher = QuestionBatcher(
+        input_dir=args.data_dir,
+        batch_size=args.batch_size,
+        question_tokenizer_name = args.question_tokenizer_name,
+        cached_QAMetaData_path = args.cached_QAMetaData_path,
+        raw_QAData_path = args.raw_QAData_path,
+        force_data_prepro = args.force_data_prepro,
+        mode = args.mode
     )
 
-    batch_size = 32
+    next_batch_func = batcher.yield_next_batch_train if args.mode == 'train' else batcher.yield_next_batch_test
+    for data in next_batch_func():
+        questions, q_embeddings, source_ent, ans_ent = data
+        question_text = batcher.translate_questions(questions)
+        ent_names = batcher.translate_entities(source_ent)
+        ans_ent_name = batcher.translate_entities(ans_ent)
 
-    print(f"Training on {len(train_df)} samples, with batch size {batch_size}")
-
-    for sample_offset_idx in tqdm(range(0, len(train_df), batch_size), desc="Training Batches", leave=False):
-        mini_batch = train_df[sample_offset_idx : sample_offset_idx + batch_size]
-
-        # Deconstruct the batch
-        questions = mini_batch["Question"].tolist()
-        query_ent = mini_batch["Query-Entity"].tolist()
-        query_rel = mini_batch["Query-Relation"].tolist()
-        answer_id = mini_batch["Answer-Entity"].tolist()
-        hops = mini_batch["Hops"].tolist() if "Hops" in mini_batch.columns else None
-        paths = mini_batch["Paths"].tolist() if "Paths" in mini_batch.columns else None
-
-        question_embeddings = data_utils.ids_to_embeddings_tf(
-            token_id_batches=questions,
-            model=question_encoder,
-            pad_id=0,                  # adjust if your PAD differs
-            add_special_tokens=True,   # set False if your IDs already include [CLS]/[SEP]
-            cls_id=101,
-            sep_id=102,
-            max_length=128,
-        )
-        # question_embeddings.shape -> [batch, hidden] (e.g., [32, 768])
-
-        # # For debugging purposes
-        # for element_id in range(len(mini_batch)):
-        #     questions_txt = question_tokenizer.decode(questions[element_id])
-        #     paths_txt = [[id2ent[head], id2rel[rel], id2ent[tail]] for head, rel, tail in paths[element_id]]
-        #     print(f"Batch {sample_offset_idx // batch_size + 1} - Questions: {questions_txt}, Query Entity: {id2ent[query_ent[element_id]]}, Query Relation: {id2rel[query_rel[element_id]]}, Answer ID: {id2ent[answer_id[element_id]]}, Hops: {hops[element_id]}, Paths: {paths_txt}")
+        for i0 in range(source_ent.shape[0]):
+            print(f"Batch Questions: {question_text[i0]}, Source Entity: {ent_names[i0]}, Answer Entity: {ans_ent_name[i0]}")
