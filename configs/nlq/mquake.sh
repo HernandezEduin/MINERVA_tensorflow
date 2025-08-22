3a8e96f913eb83272c9978209d435c105a0fcb99
HernandezEduin <eduin.ee08@nycu.edu.tw>
2025-08-22 20:07:48 +0800

    Configs (NLQ): MQuAKE

---
+++ configs/nlq/mquake.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+data_input_dir="datasets/data_preprocessed/mquake/"
+vocab_dir="datasets/data_preprocessed/mquake/vocab"
+total_iterations=40000
+eval_every=100
+path_length=4
+hidden_size=50
+embedding_size=50
+batch_size=32
+beta=0.05
+Lambda=0.05
+use_entity_embeddings=True
+train_entity_embeddings=True
+train_relation_embeddings=True
+base_output_dir="output/mquake/"
+load_model=False
+model_load_dir="null"
+raw_QAData_path="./datasets/data_preprocessed/mquake/mquake_qa_nhop.csv"
+cached_QAMetaData_path="./.cache/itl/mquake_qa_nhop.json"
+question_tokenizer_name="bert-base-uncased"
+use_beam=True
+print_path=False
+wandb_project="mquake-minerva"
+wandb_name="QAnhop-Reason4hop"
+track=True
