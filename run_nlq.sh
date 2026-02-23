8c61ea579e980e4c1d484ab989d6e2ea36dfb4f3
Eduin Hernandez <eduin.ee08@nycu.edu.tw>
2026-02-23 20:05:44 +0800

    GPU: Correcting bash run so it uses GPU

--- run_nlq.sh
+++ run_nlq.sh
@@ -2,7 +2,10 @@
 
 config="$1"
 export PYTHONPATH="."
-gpu_id=1
+gpu_id=0
+
+# ---- Make sure conda-provided CUDA/cuDNN are used (TF 2.11 expects CUDA 11.x + cuDNN 8.x) ----
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:/usr/lib/nvidia:${LD_LIBRARY_PATH:-}"
 
 cmd=(python code/model/nlq/trainer.py --config_yaml "$config")
 echo "Executing: CUDA_VISIBLE_DEVICES=$gpu_id ${cmd[*]}"
